d3dbde8064423bd0d812f366d23f3242f66fe136
Vivek <vivek_msn@hotmail.com>
2023-03-02 16:33:49 +0530

    Changes

---
+++ inc/customizer.php
@@ -0,0 +1,354 @@
+<?php 
+
+/**
+ * Fany Lab Theme Customizer
+ *
+ * @package Fancy Lab
+ */
+
+function fancy_lab_customizer( $wp_customize ){
+
+	// Copyright Section
+
+	$wp_customize->add_section(
+		'sec_copyright', array(
+			'title'			=> 'Copyright Settings',
+			'description'	=> 'Copyright Section'
+		)
+	);
+
+			// Field 1 - Copyright Text Box
+			$wp_customize->add_setting(
+				'set_copyright', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'sanitize_text_field'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_copyright', array(
+					'label'			=> 'Copyright',
+					'description'	=> 'Please, add your copyright information here',
+					'section'		=> 'sec_copyright',
+					'type'			=> 'text'
+				)
+			);
+
+	/*--------------------------------------------------------------------------------*/
+   // Slider Section
+	
+   $wp_customize->add_section(
+		'sec_slider', array(
+			'title'			=> 'Slider Settings',
+			'description'	=> 'Slider Section'
+		)
+	);
+
+		// Field 1 - Slider Page Number 1
+
+		$wp_customize->add_setting(
+			'set_slider_page1', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_page1', array(
+				'label'			=> 'Set slider page 1',
+				'description'	=> 'Set slider page 1',
+				'section'		=> 'sec_slider',
+				'type'			=> 'dropdown-pages'
+			)
+		);
+
+		// Field 2 - Slider Button Text Number 1
+
+		$wp_customize->add_setting(
+			'set_slider_button_text1', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'sanitize_text_field'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_button_text1', array(
+				'label'			=> 'Button Text for Page 1',
+				'description'	=> 'Button Text Page 1',
+				'section'		=> 'sec_slider',
+				'type'			=> 'text'
+			)
+		);
+
+		
+		// Field 3 - Slider Button URL Number 1
+
+		$wp_customize->add_setting(
+			'set_slider_button_url1', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'esc_url_raw'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_button_url1', array(
+				'label'			=> 'URL for Page 1',
+				'description'	=> 'URL for Page 1',
+				'section'		=> 'sec_slider',
+				'type'			=> 'url'
+			)
+		);
+
+		/*----------------------------------------*/
+
+		// Field 4 - Slider Page Number 2
+
+		$wp_customize->add_setting(
+			'set_slider_page2', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_page2', array(
+				'label'			=> 'Set slider page 2',
+				'description'	=> 'Set slider page 2',
+				'section'		=> 'sec_slider',
+				'type'			=> 'dropdown-pages'
+			)
+		);
+
+		// Field 5 - Slider Button Text Number 2
+
+		$wp_customize->add_setting(
+			'set_slider_button_text2', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'sanitize_text_field'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_button_text2', array(
+				'label'			=> 'Button Text for Page 2',
+				'description'	=> 'Button Text Page 2',
+				'section'		=> 'sec_slider',
+				'type'			=> 'text'
+			)
+		);
+
+		
+		// Field 6 - Slider Button URL Number 3
+
+		$wp_customize->add_setting(
+			'set_slider_button_url2', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'esc_url_raw'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_button_url2', array(
+				'label'			=> 'URL for Page 2',
+				'description'	=> 'URL for Page 2',
+				'section'		=> 'sec_slider',
+				'type'			=> 'url'
+			)
+		);
+
+		/*-----------------------------------------*/
+
+		// Field 7 - Slider Page Number 3
+
+		$wp_customize->add_setting(
+			'set_slider_page3', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_page3', array(
+				'label'			=> 'Set slider page 3',
+				'description'	=> 'Set slider page 3',
+				'section'		=> 'sec_slider',
+				'type'			=> 'dropdown-pages'
+			)
+		);
+
+		// Field 8 - Slider Button Text Number 3
+
+		$wp_customize->add_setting(
+			'set_slider_button_text3', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'sanitize_text_field'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_button_text3', array(
+				'label'			=> 'Button Text for Page 3',
+				'description'	=> 'Button Text Page 3',
+				'section'		=> 'sec_slider',
+				'type'			=> 'text'
+			)
+		);
+
+		
+		// Field 9 - Slider Button URL Number 3
+
+		$wp_customize->add_setting(
+			'set_slider_button_url3', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'esc_url_raw'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_slider_button_url3', array(
+				'label'			=> 'URL for Page 3',
+				'description'	=> 'URL for Page 3',
+				'section'		=> 'sec_slider',
+				'type'			=> 'url'
+			)
+		);
+
+	/*-------------------------------------------------------------*/
+	//Home page settings	
+
+	$wp_customize->add_section(
+		'sec_home_page', array(
+			'title'			=> 'Home Page Products and Blog settings',
+			'description'	=> 'Home Page Section'
+		)
+	);
+
+		// Field 1 - Popular Products Max Number 
+
+		$wp_customize->add_setting(
+			'set_popular_max_num', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_popular_max_num', array(
+				'label'			=> 'Popular Products Max Number',
+				'description'	=> 'Popular Products Max Number',
+				'section'		=> 'sec_home_page',
+				'type'			=> 'number'
+			)
+		);
+
+		// Field 1 - Popular Products Max Columns
+
+		$wp_customize->add_setting(
+			'set_popular_max_col', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_popular_max_col', array(
+				'label'			=> 'Popular Products Max Columns',
+				'description'	=> 'Popular Products Max Columns',
+				'section'		=> 'sec_home_page',
+				'type'			=> 'number'
+			)
+		);
+
+		// Field 1 - New Arrivals Products Max Number 
+
+		$wp_customize->add_setting(
+			'set_new_arrivals_max_num', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_new_arrivals_max_num', array(
+				'label'			=> 'New Arrivals Max Number',
+				'description'	=> 'New Arrivals Max Number',
+				'section'		=> 'sec_home_page',
+				'type'			=> 'number'
+			)
+		);
+
+		// Field 1 - New Arrivals Products Max Columns
+
+		$wp_customize->add_setting(
+			'set_new_arrivals_max_col', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_new_arrivals_max_col', array(
+				'label'			=> 'New Arrivals Max Columns',
+				'description'	=> 'New Arrivals Max Columns',
+				'section'		=> 'sec_home_page',
+				'type'			=> 'number'
+			)
+		);
+
+		// Deal of the Week Checkbox
+
+		$wp_customize->add_setting(
+			'set_deal_show', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'fancy_lab_sanitize_checkbox'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_deal_show', array(
+				'label'			=> 'Show Deal of the Week',
+				'section'		=> 'sec_home_page',
+				'type'			=> 'checkbox'
+			)
+		);
+
+		// Deal of the Week Product ID
+
+		$wp_customize->add_setting(
+			'set_deal', array(
+				'type'					=> 'theme_mod',
+				'default'				=> '',
+				'sanitize_callback'		=> 'absint'
+			)
+		);
+
+		$wp_customize->add_control(
+			'set_deal', array(
+				'label'			=> 'Deal of the Week Product ID',
+				'description'	=> 'Product ID to Display',
+				'section'		=> 'sec_home_page',
+				'type'			=> 'number'
+			)
+		);
+
+}
+add_action( 'customize_register', 'fancy_lab_customizer' );
+
+function fancy_lab_sanitize_checkbox( $checked ){
+	return( ( isset ( $checked ) && true == $checked ) ? true : false );
+}
---
+++ template-home.php
@@ -0,0 +1,128 @@
+<?php 
+
+/*
+Template Name: Home Page
+*/
+
+get_header(); ?>
+
+		<div class="content-area">
+			<main>
+				<section class="slider">
+					<div class="flexslider">
+					  <ul class="slides">
+						<?php
+						// Getting data from Customizer to display the Slider Section
+						for($i=1; $i < 4; $i++):
+							$slider_page[$i]				= get_theme_mod( 'set_slider_page' . $i );
+							$slider_button_text[$i]			= get_theme_mod( 'set_slider_button_text' . $i );
+							$slider_button_url[$i]          = get_theme_mod( 'set_slider_button_url' . $i );
+						endfor;
+
+						$args = array(
+							'post_type' 		=>	'page',
+							'posts_per_page'	=> 3,
+							'post__in'			=> $slider_page,
+							'orderby'			=> 'post__in'
+						);
+
+						$slider_loop = new WP_Query( $args );
+						$j = 1;
+
+						if( $slider_loop->have_posts() ):
+							while( $slider_loop->have_posts() ):
+								$slider_loop->the_post();
+						?>
+					    <li>
+							<?php the_post_thumbnail( 'fancy-lab-slider', array( 'class' => 'img-fluid' ) ); ?>
+							<div class="container">
+								<div class="slider-details-container">
+									<div class="slider-title">
+										<h1><?php the_title();?></h1>
+									</div>
+									<div class="slider-description">
+										<div class="subtitle"><?php the_content(); ?></div>
+										<a class="link" href="<?php echo $slider_button_url[$j]; ?>"><?php echo $slider_button_text[$j]; ?></a>
+									</div>
+								</div>
+							</div>
+					    </li>
+					    <?php
+						$j++;
+						endwhile;
+						wp_reset_postdata();
+					    endif;
+						 ?>
+					  </ul>
+					</div>
+				</section>
+				<section class="popular-products">
+					<?php
+					$popular_limit		= get_theme_mod( 'set_popular_max_num', 4 );
+					$popular_col		= get_theme_mod( 'set_popular_max_col', 4 );
+					$arrivals_limit		= get_theme_mod( 'set_new_arrivals_max_num', 4 );
+					$arrivals_col		= get_theme_mod( 'set_new_arrivals_max_col', 4 );
+
+					?>
+					<div class="container">
+						<h2>Popular Products</h2>
+						<?php echo do_shortcode( '[products limit=" ' . $popular_limit . '" columns=" ' . $popular_col . '" orderby="popularity"]' ); ?>
+					</div>
+				</section>
+				<section class="new-arrivals">
+					<div class="container">
+						<h2>New Arrivals</h2>
+						<?php echo do_shortcode( '[products limit=" ' . $arrivals_limit . ' " columns=" ' . $arrivals_col . ' " orderby="date"]' ); ?>
+					</div>
+				</section>
+				<?php
+
+				$showdeal		= get_theme_mod( 'set_deal_show', 0 );
+				$deal			= get_theme_mod( 'set_deal ' );
+				$currency		= get_woocommerce_currency_symbol();
+				$regular		= get_post_meta( $deal, '_regular_price', true );
+				$sale 			= get_post_meta( $deal, '_sale_price', true );
+
+				if( $showdeal == 1 && ( !empty( $deal ) ) ):
+					$discount_percentage =absint( 100 - ( ( $sale/$regular ) * 100 ) );
+				?>	
+				<section class="deal-of-the-week">
+					<div class="container">
+						<h2>Deal of the Week</h2>
+						<div class="row d-flex align-items-centre">
+							<div class="deal-img col-md-6 col-12 ml-auto text-centre">
+								<?php echo get_the_post_thumbnail( $deal, 'large',array( 'class' => 'img-fluid' ) ); ?>
+							</div>
+							<div class="deal-desc col-md-4 col-12 mr-auto text-centre">
+
+							</div>
+					    </div>
+					</div>
+				</section>
+				<?php endif; ?>
+				<section class="lab-blog">
+					<div class="container">
+						<div class="row">
+							<?php 
+								// If there are any posts
+								if( have_posts() ):
+
+									// Load posts loop
+									while( have_posts() ): the_post();
+										?>
+											<article>
+												<h2><?php the_title(); ?></h2>
+												<div><?php the_content(); ?></div>
+											</article>
+										<?php
+									endwhile;
+								else:
+							?>
+								<p>Nothing to display.</p>
+							<?php endif; ?>
+						</div>
+					</div>
+				</section>
+			</main>
+		</div>
+<?php get_footer(); ?>
