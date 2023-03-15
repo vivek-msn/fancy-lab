0880cf2a0c154263b25e787af457e9ce4e9ad74c
Vivek <vivek_msn@hotmail.com>
2023-03-15 14:27:45 +0530

    In Blog section we add function to display category ,display featured image,permalink,limit of words to display

--- index.php
+++ index.php
@@ -25,9 +25,30 @@ get_header();
 								// Load posts loop
 								while( have_posts() ): the_post();
 									?>
-										<article>
-											<h2><?php the_title(); ?></h2>
-											<div><?php the_content(); ?></div>
+										<article <?php post_class(); ?>>
+											<h2>
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h2>
+											<div class="post-thumbnail">
+												<?php
+												if( has_post_thumbnail() ):
+													the_post_thumbnail( 'fancy-lab-blog',array( 'class' => 'img-fluid' ) );
+												endif;
+												?>
+											</div>
+											<div class="meta">
+												<p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+												<br/>
+												<?php if( has_category() ): ?>
+													Categories: <span><?php the_category( '' ); ?></span>
+												<?php endif; ?>
+												<br/>
+												<?php if( has_tag() ): ?>
+													Tags: <span><?php the_tags( '', ' , '); ?></span>
+												<?php endif;?>
+											    </p>
+											</div>
+											<div><?php the_excerpt(); ?></div>
 										</article>
 									<?php
 								endwhile;
