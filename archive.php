c55b4fec7075281b44f82713ca48e3ce527cf251
Vivek <vivek_msn@hotmail.com>
2023-03-16 01:20:05 +0530

    add archive.php file and modified or add more code in same file

--- archive.php
+++ archive.php
@@ -14,6 +14,9 @@ get_header();
 				<div class="container">
 					<div class="row">
 						<?php 
+
+							the_archive_title( '<h1 class="article-title">', '</h1>' );
+
 							// If there are any posts
 							if( have_posts() ):
 
@@ -25,32 +28,35 @@ get_header();
 												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 											</h2>
 											<div class="post-thumbnail">
-												<?php
+												<?php 
 												if( has_post_thumbnail() ):
-													the_post_thumbnail( 'fancy-lab-blog',array( 'class' => 'img-fluid' ) );
+													the_post_thumbnail( 'fancy-lab-blog', array( 'class' => 'img-fluid' ) );
 												endif;
 												?>
 											</div>
 											<div class="meta">
 												<p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
-												<br/>
+												<br />
 												<?php if( has_category() ): ?>
-													Categories: <span><?php the_category( '' ); ?></span>
+													Categories: <span><?php the_category( ' ' ); ?></span>
 												<?php endif; ?>
-												<br/>
+												<br />
 												<?php if( has_tag() ): ?>
-													Tags: <span><?php the_tags( '', ' , '); ?></span>
-												<?php endif;?>
-											    </p>
+													Tags: <span><?php the_tags( '', ', ' ); ?></span>
+												<?php endif; ?>
+												</p>
 											</div>
 											<div><?php the_excerpt(); ?></div>
 										</article>
 									<?php
 								endwhile;
+
+								// We're using numeric page navigation here.
 								the_posts_pagination( array(
-									'prev_text' => 'Previous',
-									'next_text' => 'Next',
+									'prev_text'		=> 'Previous',
+									'next_text'		=> 'Next',
 								));
+								
 							else:
 						?>
 							<p>Nothing to display.</p>
