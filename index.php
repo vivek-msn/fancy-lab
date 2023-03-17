b9a550c3838b0b9a581de59bb0074e96984cd80b
Vivek <vivek_msn@hotmail.com>
2023-03-17 12:25:58 +0530

    only code overide to check issues we face is solved or not by doing this

--- index.php
+++ index.php
@@ -30,32 +30,35 @@ get_header();
 												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 											</h2>
 											<div class="post-thumbnail">
-												<?php
+												<?php 
 												if( has_post_thumbnail() ):
-													the_post_thumbnail( 'fancy-lab-blog',array( 'class' => 'img-fluid' ) );
+													the_post_thumbnail( 'fancy-lab-blog', array( 'class' => 'img-fluid' ) );
 												endif;
 												?>
 											</div>
 											<div class="meta">
 												<p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
-												<br/>
+												<br />
 												<?php if( has_category() ): ?>
-													Categories: <span><?php the_category( '' ); ?></span>
+													Categories: <span><?php the_category( ' ' ); ?></span>
 												<?php endif; ?>
-												<br/>
+												<br />
 												<?php if( has_tag() ): ?>
-													Tags: <span><?php the_tags( '', ' , '); ?></span>
-												<?php endif;?>
-											    </p>
+													Tags: <span><?php the_tags( '', ', ' ); ?></span>
+												<?php endif; ?>
+												</p>
 											</div>
 											<div><?php the_excerpt(); ?></div>
 										</article>
 									<?php
 								endwhile;
+
+								// We're using numeric page navigation here.
 								the_posts_pagination( array(
-									'prev_text' => 'Previous',
-									'next_text' => 'Next',
+									'prev_text'		=> 'Previous',
+									'next_text'		=> 'Next',
 								));
+								
 							else:
 						?>
 							<p>Nothing to display.</p>
--- single.php
+++ single.php
@@ -10,51 +10,52 @@
 get_header();
 ?>
 <div id="primary" class="content-area">
-    <div id="main">
-        <div class="container">
-            <div class="row">
-                <?php
-                    while( have_posts() ): the_post();
-                        ?>
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                            <header>
-                                <h1><?php the_title(); ?></h1>
-                                    <div class="meta">
-                                        <p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?><br />
-                                        Categories: <span><?php the_category( ' ' ); ?><br/>
-                                        <?php if(has_tag()): ?>
-                                            Tags: <span><?php the_tags( '', ', '); ?></span>
-                                        <?php endif; ?>
-                                        </p>                            
-                                    </div>        
-                                    <div class="post-thumbnail">
-                                        <?php 
-                                        if( has_post_thumbnail() ): 
-                                            the_post_thumbnail( 'fancy-lab-blog', array( 'class' => 'img-fluid') );
-                                        endif;
-                                        ?>
-                                    </div>
-                            </header>
-                            <div class="content">
-                                <?php
-                                wp_link_pages(
-                                    array(
-                                        'before'    => '<p class="inner-pagination">' . 'Pages',
-                                        'after'     => '</p>' ,
-                                    )
-                                );
-                                ?>
+	<div id="main">
+		<div class="container">
+			<div class="row">
+				<?php 
+					while( have_posts() ): the_post();
+						?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
+						    <header>
+						        <h1><?php the_title(); ?></h1>
+						        <div class="meta">
+						            <p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?><br />
+						            Categories: <span><?php the_category( ' ' ); ?><br/>
+						            <?php if(has_tag()): ?>
+						                Tags: <span><?php the_tags( '', ', '); ?></span>
+						            <?php endif; ?>
+						            </p>                            
+						        </div>        
+						        <div class="post-thumbnail">
+						            <?php 
+						            if( has_post_thumbnail() ): 
+						                the_post_thumbnail( 'fancy-lab-blog', array( 'class' => 'img-fluid') );
+						            endif;
+						            ?>
+						        </div>
+						    </header>    
+						    <div class="content">
+						    	<?php 
+						    	wp_link_pages(
+						    		array(
+						    			'before'	=> '<p class="inner-pagination">' . 'Pages',
+						    			'after'		=> '</p>',
+						    		)
+						    	);
+						    	?>
 						        <?php the_content(); ?>
 						    </div>
-                        </article>
-                        <?php
-                        if( comments_open() || get_comments_number() ):
-                            comments_template();
-                        endif;
-                    endwhile;
-                ?>
-            </div>
-        </div>
-    </div>
+						</article>						
+						<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if( comments_open() || get_comments_number() ):
+							comments_template();
+						endif;
+					endwhile;
+				?>
+			</div>
+		</div>
+	</div>
 </div>
 <?php get_footer(); ?>
